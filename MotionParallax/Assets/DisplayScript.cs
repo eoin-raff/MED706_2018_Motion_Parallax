ef0efaf50fc78a292660992caa130744dee6f04d
Anders Christiansen <aschristiansen@gmail.com>
2018-10-15 12:21:05 +0200

    Created initial multiple camera setup, need cylindrical unwrap

---
+++ MotionParallax/Assets/DisplayScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayScript : MonoBehaviour {
+
+
+	void Start () {
+        /* Activates all cameras currently in the scene,
+         * each rendering to a different display.
+         */
+
+        Debug.Log("Displays connected: " + (Display.displays.Length - 1));
+        int i = 1;
+        while(Display.displays.Length > i)
+        {
+            Display.displays[i].Activate();
+            Debug.Log("Found additional display " + i);
+            i++;
+        }
+	}
+}
