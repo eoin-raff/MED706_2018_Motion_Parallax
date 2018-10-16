0c716c8e7caae10219066c27aef7f7ada5277703
Anders Christiansen <aschristiansen@gmail.com>
2018-10-16 16:25:00 +0200

    No stretch multiple cameras, more narrow per-screen FOV
    I use a script to narrow the FOV horizontally, so that the total FOV is the same (90?) but individual FOV per screen is smaller. Then, simply lining up the frustums and mapping one camera to one screen means that there are no overlaps.

---
+++ MotionParallax/Assets/Scripts/CameraFrustumGizmo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFrustumGizmo : MonoBehaviour
+{
+    public virtual void OnDrawGizmos()
+    {
+        Matrix4x4 temp = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        Camera camera = GetComponent<Camera>();
+        if (camera.orthographic)
+        {
+            float spread = camera.farClipPlane - camera.nearClipPlane;
+            float center = (camera.farClipPlane + camera.nearClipPlane) * 0.5f;
+            Gizmos.DrawWireCube(new Vector3(0, 0, center), new Vector3(camera.orthographicSize * 2 * camera.aspect, camera.orthographicSize * 2, spread));
+        }
+        else
+        {
+            Gizmos.DrawFrustum(new Vector3(0, 0, (camera.nearClipPlane)), camera.fieldOfView, camera.farClipPlane, camera.nearClipPlane, camera.aspect);
+        }
+        Gizmos.matrix = temp;
+    }
+}
---
+++ MotionParallax/Assets/Scripts/SetObliqueness.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObliqueness : MonoBehaviour {
+
+    public float horizObl, vertObl;
+    private Camera _camera;
+    private Matrix4x4 mat;
+
+    void Start ()
+    {
+        _camera = GetComponent<Camera>();
+        mat = _camera.projectionMatrix;
+    }
+
+    void Update()
+    {
+        mat[0, 2] = horizObl;
+        mat[1, 2] = vertObl;
+        _camera.projectionMatrix = mat;
+    }
+}
