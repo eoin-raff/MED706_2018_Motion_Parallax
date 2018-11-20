a6be8e29ebe9ed020b15c9441a195be820be6a5b
EoinRaff <eoinrafferty@gmail.com>
2018-11-20 17:58:45 +0100

    All displays active, flat near clipping plane
    Horizontal stretching is all right, but might be fixed with a curved near clipping plane

---
+++ MotionParallax/Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public MotionParallax motionParallax;
+    public GameObject calibrationObject;
+
+    // Use this for initialization
+    private bool _calibratingCamera = true;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MotionParallax/Assets/Scripts/RegisterDisplays.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RegisterDisplays : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Displays connected: " + Display.displays.Length);
+        // Display.displays[0] is the primary, default display and is always ON.
+        // Check if additional displays are available and activate each.
+        for (int i = 1; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+    }
+}
