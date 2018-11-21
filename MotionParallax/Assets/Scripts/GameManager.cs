bc5a06e964785375d12ca9ee356fa4cf5ed908da
Anders Christiansen <aschristiansen@gmail.com>
2018-11-21 15:15:23 +0100

    allowed control of Zmap via GM script

--- MotionParallax/Assets/Scripts/GameManager.cs
+++ MotionParallax/Assets/Scripts/GameManager.cs
@@ -8,9 +8,14 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager instance;
     private bool _calibrating;
+    private bool _accessZMapFActor;
     private int _scene;
     private float _yOffset;
     private Vector3 _verticalOffset;
+    [SerializeField]
+    private float _zMapStep = 0.05f;
+    [SerializeField][Range(0, 1)]
+    private float _zMapFactor = 0.5f;
 
     private void Awake()
     {
@@ -25,8 +30,10 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
         }
 
+        //initalize variables
         _scene = 0;
         _calibrating = true;
+        _accessZMapFActor = false;
         _verticalOffset = Vector3.zero;
     }
 
@@ -42,34 +49,62 @@ public class GameManager : MonoBehaviour {
                 LoadNextScene();
             }
         }
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            Debug.Log("Can Change Zmap");
+            CalibrateZMapFactor();
+        }
 		
 	}
 
-    private void LoadNextScene()
+    private void CalibrateZMapFactor()
     {
-        throw new NotImplementedException();
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Zmap Up");
+            _zMapFactor += _zMapStep;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Zmap Down");
+
+            _zMapFactor -= _zMapStep;
+        }
+        Mathf.Clamp(_zMapStep, 0, 1);
+
     }
+
+
 
     private void CalibrateVerticalPosition()
     {
         _yOffset = 0;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            Debug.Log("Up");
             _yOffset = 0.01f;
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
-            Debug.Log("Down");
             _yOffset = -0.01f;
         }
         _verticalOffset.y += _yOffset;
-        Debug.Log(_verticalOffset);
+    }
+
+    private void LoadNextScene()
+    {
+        throw new NotImplementedException();
     }
 
     public Vector3 VerticalOffset
     {
         get { return _verticalOffset; }
         set { _verticalOffset = value; }
+    }
+
+    public float ZMapFactor
+    {
+        get { return _zMapFactor; }
+        set { _zMapFactor = value; }
     }
 }
