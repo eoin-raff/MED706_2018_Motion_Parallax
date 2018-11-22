cd0b1455cd35442248ff8bc64cbb0932f0ffdff4
Anders Christiansen <aschristiansen@gmail.com>
2018-11-22 14:26:06 +0100

    Baked Lightmaps for Test Scene and programmed Test Behaviour in GM

--- MotionParallax/Assets/Scripts/GameManager.cs
+++ MotionParallax/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -17,6 +19,27 @@ public class GameManager : MonoBehaviour {
     [SerializeField][Range(0, 1)]
     private float _zMapFactor = 0.5f;
 
+    #region Z Test Variables
+    private string _path;
+    private string _filename;
+    private bool _testing;
+    private static int _participant;
+    private bool _staircaseTestA = true;
+    private float _zA = 1.0f;
+    private float _zB = 0.0f;
+    private float _zTemp;
+    private List<float> _aValues;
+    private List<float> _bValues;
+    [SerializeField]
+    private int _checkedIterations = 3;
+    private bool _changedDirection;
+    private bool _finishedA;
+    private bool _finishedB;
+    private bool[] _checkChangesA;
+    private bool[] _checkChangesB;
+
+    #endregion
+
     private void Awake()
     {
         //Singleton
@@ -32,31 +55,165 @@ public class GameManager : MonoBehaviour {
 
         //initalize variables
         _scene = 0;
+        _participant = 0;
         _verticalOffset = Vector3.zero;
+        _aValues = new List<float>();
+        _bValues = new List<float>();
+        _path = Application.dataPath + "/CSV/";
+        _testing = false;
+        _changedDirection = false;
+        _finishedA = false;
+        _finishedB = false;
+        _checkChangesA = new bool[_checkedIterations * 2];
+        _checkChangesB = new bool[_checkedIterations * 2];
     }
 
     void Update () {
+
+        #region Calibration
         SetObjectsActive(_calibrationReference, false);
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
         {
             SetObjectsActive(_calibrationReference, true);
             CalibrateVerticalPosition();
         }
+        #endregion
+
+        #region Staircase Test
+        //TODO: 
+        /* DONEAutomatically detect test end
+         * Automatically detect if direction was changed
+         * Test script
+         */
+        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!_testing)
+            {
+                StartTest();
+            }
+            else
+            {
+                StopTest();
+            }
+        }
 
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            Debug.Log("Can Change Zmap");
             CalibrateZMapFactor();
         }
 
+        CheckForEndCondition();
+        #endregion
+
+        #region Level Select
         if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) &&
             (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))&&
             Input.GetKeyDown(KeyCode.L))
         {
             LoadNextScene();
         }
-
+        #endregion
     }
+
+    #region Z Map Test
+    private void CalibrateZMapFactor()
+    {
+        if (_staircaseTestA)
+            _zTemp = _zA;
+        else
+            _zTemp = _zB;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Zmap Up");
+            _zTemp += _zMapStep;
+            SwitchStaircase();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Zmap Down");
+
+            _zTemp -= _zMapStep;
+            SwitchStaircase();
+        }
+        Mathf.Clamp(_zMapStep, 0, 1);
+    }
+
+    private void SwitchStaircase()
+    {
+        if (_staircaseTestA)
+        {
+            _zA = _zTemp;
+            _aValues.Add(_zA);
+        }
+        else
+        {
+            _zB = _zTemp;
+            _bValues.Add(_zB);
+        }
+        _staircaseTestA = !_staircaseTestA;
+    }
+
+
+    private void StartTest()
+    {
+        _participant++;
+        DateTime dt = DateTime.Now;
+        String _date = dt.ToString("MM-DD");
+        _filename = "Z_Factor_" + _date + "_Participant_" + _participant;
+        _zA = 1.0f;
+        _zB = 0.0f;
+        _staircaseTestA = true;
+    }
+
+    private void StopTest()
+    {
+        PrintTestData();
+        //load Post-Test-level
+    }
+
+    private void PrintTestData()
+    {
+        String _dataA = "";
+        String _dataB = "";
+        for (int i = 0; i < _aValues.Count; i++)
+        {
+            _dataA += _aValues[i] + ",";
+        }
+        for (int i = 0; i < _bValues.Count; i++)
+        {
+            _dataB += _bValues[i] + ",";
+        }
+        StreamWriter csvWriter = File.CreateText(_path + "/" + _filename);
+        csvWriter.WriteLine(_dataA);
+        csvWriter.WriteLine(_dataB);
+        csvWriter.Close();
+    }
+
+    private void CheckForEndCondition()
+    {
+        SearchForFalse(_finishedA, _checkChangesA);
+        SearchForFalse(_finishedB, _checkChangesB);
+        if (_finishedA && _finishedB)
+        {
+            _testing = false;
+            StopTest();
+        }
+    }
+
+    private void SearchForFalse(bool a, bool[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == false)
+                return;
+        }
+        a = true;
+    }
+    #endregion
+
+    #region Calibration Methods
 
     private void SetObjectsActive(GameObject[] a, bool b)
     {
@@ -65,25 +222,6 @@ public class GameManager : MonoBehaviour {
             a[i].SetActive(b);
         }
     }
-
-    private void CalibrateZMapFactor()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("Zmap Up");
-            _zMapFactor += _zMapStep;
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Debug.Log("Zmap Down");
-
-            _zMapFactor -= _zMapStep;
-        }
-        Mathf.Clamp(_zMapStep, 0, 1);
-
-    }
-
-
 
     private void CalibrateVerticalPosition()
     {
@@ -98,14 +236,18 @@ public class GameManager : MonoBehaviour {
         }
         _verticalOffset.y += _yOffset;
     }
-
+    #endregion
+    
+    #region Scene Management Methods
     private void LoadNextScene()
     {
         _scene++;
         SceneManager.LoadScene(_scene);
         throw new NotImplementedException();
     }
+    #endregion
 
+    #region Set/Get
     public Vector3 VerticalOffset
     {
         get { return _verticalOffset; }
@@ -117,4 +259,6 @@ public class GameManager : MonoBehaviour {
         get { return _zMapFactor; }
         set { _zMapFactor = value; }
     }
+    #endregion
+
 }
