e4dbd65e9a27755cfc40c4a5ec8c8fdf426b6c1c
EoinRaff <eoinrafferty@gmail.com>
2018-11-27 10:19:52 +0100

    Added flying car

---
+++ MotionParallax/Assets/CyberpunkFlyingCar/Scripts/play_anim_on_ui_button.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine; using UnityEngine.UI; 
+using System.Collections;
+
+public class play_anim_on_ui_button : MonoBehaviour {
+	public Button Text;
+	public Animation anim;
+	public Canvas yourcanvas;
+
+
+
+	void Start () 
+	{
+		Text = Text.GetComponent<Button> ();
+		anim = GetComponent<Animation>();
+		yourcanvas.enabled = true;
+	}
+
+
+	public void Press() 
+
+	{
+		Text.enabled = true;
+		GetComponent<Animation>().CrossFade ("LeftFrontDoorOpen");
+
+
+	}
+}
---
+++ MotionParallax/Assets/CyberpunkFlyingCar/Scripts/toggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggle : MonoBehaviour {
+
+	public GameObject myobject;
+	public bool activateme;
+
+	void Start()
+	{
+		myobject.SetActive (false);
+		activateme = true;
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+		if (activateme == true)
+		{
+			myobject.SetActive (true);
+	        activateme = false;
+
+		}
+
+		if (activateme == false)
+		{
+			myobject.SetActive (false);
+			activateme = true;
+		}
+			
+	}
+}
