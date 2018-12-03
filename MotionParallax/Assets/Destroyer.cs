8dda7b230e73ae193e3cd76de2c05c5e41848dfe
Anders Christiansen <aschristiansen@gmail.com>
2018-12-03 15:43:15 +0100

    added path and adjusted audio for spaceship

---
+++ MotionParallax/Assets/Destroyer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("triggered");
+        if (other.gameObject.tag == "Destroy")
+        {
+            print("BOOM");
+            Destroy(other.gameObject);
+        }
+        
+    }
+}
